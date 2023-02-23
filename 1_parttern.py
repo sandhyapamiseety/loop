c5f8ae5d24f503a01f78a0ec87a457d5417ed4ae
sandhyapamisetty <115785545+sandhyapamisetty@users.noreply.github.com>
2023-02-23 11:29:19 +0530

    Add files via upload

---
+++ 1_parttern.py
@@ -0,0 +1,28 @@
+#  1
+#  1 2
+#  1 2 3 
+#  1 2 3 4
+#  1 2 3 4 5 
+
+
+i=1
+while i<=5:
+    j=1
+    while j<=i:
+        print(j,end=" ")
+        j=j+1
+    i=i+1
+    print()
+    
+    
+#  # for loop
+    
+# for i in range(0,7,+1):
+#     for j in range(1,i,+1):
+#         print(j,end=" ")
+#     print()
+        
+
+
+    
+    
